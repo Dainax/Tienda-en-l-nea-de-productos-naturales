87a0bcf1158dd71c93cbb1330392d0e21b3d31a1
Diego Mendez <130942247+Dainax@users.noreply.github.com>
2023-07-01 06:36:52 -0400

    Desglose.py

--- Desglose.py
+++ Desglose.py
@@ -1,9 +1,11 @@
+from Motorizado import Motorizado
 class Desglose:
-    def __init__(self, subtotal: float, descuento: int, iva: float, igtf: float, total: float ):
+    def __init__(self, subtotal: float, descuento: int, iva: float, igtf: float, envio: int, total: float ):
         self.subtotal = subtotal
         self.descuento = descuento
         self.iva = iva
         self.igtf = igtf
+        self.envio = envio
         self.total = total
         pass
     
@@ -13,5 +15,6 @@ class Desglose:
         Descuentos: {self.descuento}%
         IVA: 16% - ${self.iva} 
         IGTF: 3% {self.igtf}
+        Envio: {self.envio}
         Total: ${self.total}
         """
