cf317d2b8aa3696f5cfc09f140e194266adfef62
Diego Mendez <130942247+Dainax@users.noreply.github.com>
2023-07-05 02:47:40 -0400

    Producto.py

--- Producto.py
+++ Producto.py
@@ -16,3 +16,21 @@ class Producto:
     Inventario: {self.quantity}
     """
   
+class Productocliente:
+
+  def __init__(self, name: str, description: str, price: int, category: str,):
+    self.name = name
+    self.description = description
+    self.price = price
+    self.category = category
+
+  def show(self):
+    return f"""
+    Nombre: {self.name}
+    Descripcion: {self.description}
+    Precio: ${self.price}
+    Categoria: {self.category} 
+    """
+  
+  
+  
